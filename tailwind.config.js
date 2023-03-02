1cd91d98cf4e3f4b0719dae030bbdec829c0f029
Vinicius Pereira de Souza <vinicius.pereira.souza98@gmail.com>
2023-03-01 23:23:18 -0300

    projeto final do curso de tailwind completo

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        arial: "Arial",
+      },
+      colors: {
+        "darkblue-500": "#485769",
+        "darkblue-600": "#37475a",
+        "darkblue-700": "#232f3e",
+        "darkblue-900": "#191e26",
+        "darkblue-950": "#131a22",
+        "gold-100": "#febd69",
+        "gold-200": "#faa63a",
+        "orange-650": "#e47911",
+        "yellow-350": "#f0c148",
+      },
+      spacing: {
+        7: "1.75rem",
+        "75px": "75px",
+        "210px": "210px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
